d6c21d321e533999f45d6402d51897aab9d89d1e
DavletgareevaAR <adavletgareeva@js.team>
2023-04-26 16:42:42 +0300

    HW-9: fix

--- hw9-library-h2/src/main/java/ru/otus/spring/hw9/Hw9LibraryH2Application.java
+++ hw9-library-h2/src/main/java/ru/otus/spring/hw9/Hw9LibraryH2Application.java
@@ -1,20 +1,19 @@
 package ru.otus.spring.hw9;
 
+
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.sql.SQLException;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.otus.spring.hw9.dao"})
 public class Hw9LibraryH2Application {
 
-	public static void main(String[] args) throws SQLException {
-		SpringApplication.run(Hw9LibraryH2Application.class, args);
-		Console.main(args);
-	}
+    public static void main(String[] args) throws SQLException {
+        SpringApplication.run(Hw9LibraryH2Application.class, args);
+        Console.main(args);
+    }
 
 
 }
